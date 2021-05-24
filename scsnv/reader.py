6599ee0198c0716fa1182753e91dd891c6de03ab
illumination-k <illumination.k.27@gmail.com>
2021-05-24 17:23:46 +0900

    add dependencies, test data and reader

--- scsnv/reader.py
+++ scsnv/reader.py
@@ -1,1 +1,16 @@
 from anndata import Anndata
+from scipy.io import mmread
+import pandas as pd
+
+
+def read_vcf(vcf_path: str) -> pd.DataFrame:
+    df = pd.read_csv(vcf_path, comment="#", header=None)
+    columns = []
+    df.columns = columns
+    return df
+
+def read_mat(mat_path: str, vcf_path: str) -> Anndata:
+    mm = mmread(mat_path)
+    vcf = read_vcf(vcf_path=vcf_path)
+    return Anndata(X=mm)
+
